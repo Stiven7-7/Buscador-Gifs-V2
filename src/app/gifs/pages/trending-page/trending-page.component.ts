2a61e1fcf1a95c6df80e6adb0b3ad40a18530f3f
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2025-02-27 11:53:32 -0500

    Add scroll state management for trending GIFs and adjust layout

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,16 +1,24 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
-import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { ScrollStateService } from './../../shared/services/scroll-state.service';
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-trending-page',
-  //imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
+
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
 
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.tredingScrollState();
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
@@ -20,6 +28,8 @@ export default class TrendingPageComponent {
     const scrollHeight = scrollDiv.scrollHeight;
 
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    this.scrollStateService.tredingScrollState.set(scrollTop);
 
     if (isAtBottom) {
       this.gifService.loadTrendingGifs();
