0a8e29638fd543dc7fd79b1dd1cb78ea132dc395
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2025-02-26 19:07:06 -0500

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'TIwH2wUTmjiA8T6pZp2fJEDxUWrRG0ml',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
